174a2ec0a4eaed2043273239597b698ee485ac30
ITsPOLY <ukolova_p@inbox.ru>
2024-05-26 02:19:23 +0300

    module_6_2

--- src/components/constants/const.tsx
+++ src/components/constants/const.tsx
@@ -1,3 +1,17 @@
-export const Settings = {
-  placesCount: 312
+import { Offer } from './../types/offer';
+
+const getSorting = (offers: Offer[], sortingType: string): Offer[] | undefined => {
+  const defaultOffers = offers.slice();
+  switch (sortingType) {
+    case 'Popular':
+      return offers;
+    case 'Price: low to high':
+      return defaultOffers.sort((a, b) => a.price - b.price);
+    case 'Price: high to low':
+      return defaultOffers.sort((a, b) => b.price - a.price);
+    case 'Top rated first':
+      return defaultOffers.sort((a, b) => b.rating - a.rating);
+  }
 };
+
+export default getSorting;
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,16 +2,21 @@ import {createReducer} from '@reduxjs/toolkit';
 import { Offer, City} from './../components/types/offer';
 import { cities } from './../components/mocks/cities';
 import { offers } from './../components/mocks/offers';
-import { changeCity, getOffers } from './action';
+import { changeCity, getOffers, changeSorting, changeSelectedPoint } from '../store/action';
+import { Point } from '../components/types/point';
 
 type StateType = {
   city: City;
   offers: Offer[];
+  sortingType: string;
+  selectedPoint: Point | undefined;
 }
 
 const initialState: StateType = {
   city: cities[0],
-  offers: offers
+  offers: offers,
+  sortingType: 'Popular',
+  selectedPoint: undefined
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -21,6 +26,12 @@ const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(changeCity, (state, action) => {
       state.city = action.payload;
+    })
+    .addCase(changeSorting, (state, action) => {
+      state.sortingType = action.payload;
+    })
+    .addCase(changeSelectedPoint, (state, action) => {
+      state.selectedPoint = action.payload;
     });
 });
 
